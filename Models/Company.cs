44ef9a16f64d968c6b7432f16b9c58e4474c2d8c
unknown <dejanstoilkov68@gmail.com>
2021-12-04 16:33:05 +0100

    modified views and validation

--- Models/Company.cs
+++ Models/Company.cs
@@ -5,7 +5,11 @@ namespace WebApiCRUD.Models
     public class Company
     {
         //[Key]
+
+        [Required]
         public int CompanyId { get; set; }
+
+        [Required]
         public string CompanyName { get; set;}
 
     }
