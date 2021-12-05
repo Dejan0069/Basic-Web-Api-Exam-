d957834fa5303fd818e1bb064f688694fc0da1ad
unknown <dejanstoilkov68@gmail.com>
2021-12-06 00:46:26 +0100

    add controler for contact

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiCRUD.Data;
+using WebApiCRUD.Models;
 
 namespace WebApiCRUD.Controllers
 {
     public class ContactController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public ContactController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Contact> contactList = _db.Contact;
+            //IEnumerable<Company> objList = _db.Company;
+            return View(contactList);
         }
     }
 }
